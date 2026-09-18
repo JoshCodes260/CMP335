889870e02c4712cd70d525e709b1c96c54029bf1
JoshCodes <Joshua.kleinsasser@doane.edu>
2026-09-18 13:02:12 -0500

    v1 cipher

--- Ceasar's Cipher/cip.js
+++ Ceasar's Cipher/cip.js
@@ -1,27 +1,84 @@
 async function getBaconipsum() {
   // first build the API call string by starting with the URL
-  var apiString = "https://baconipsum.com/api/";
+  let apiString = "https://baconipsum.com/api/";
   // next add the parameters to the string using the drop down lists
-  var theNewParagraphs = document.getElementById("newParagraphs").value;
-  apiString = apiString + "?type=meat&paras=" + theNewParagraphs;
+  let theNewParagraphs = document.getElementById("newParagraphs").value;
+  let theNewType = document.getElementById("newType").value;
+  apiString = apiString + "?type=" + theNewType + "&paras=" + theNewParagraphs;
   alert(apiString);  // show the API string
 
   // now make the API call to the web service using the string and store what is returned in response
-  var response = await fetch(apiString);
+  let response = await fetch(apiString);
 
-  // finally, print the response in the various formats
+  // finally, print the response in the letious formats
   document.getElementById("myRawData").innerHTML = "";   // clear what was previously shown
   document.getElementById("myFormattedData").innerHTML = "";   // clear what was previously shown
-
-  var jsonData = await response.json();  // read the response as JSON
+  document.getElementById("myEncryptedData").innerHTML = "";
+  let jsonData = await response.json();  // read the response as JSON
   
   // stringify and print out the JSON object in the RawData section
   document.getElementById("myRawData").innerHTML = JSON.stringify(jsonData);
  
   // loop through the JSON object one paragraph at a time and print each in the FormattedData section
-  for (var para in jsonData) {   
-      document.getElementById("myFormattedData").innerHTML += "<p>" + jsonData[para] + "</p>";
+
+
+for (let para in jsonData) {
+
+    document.getElementById("myFormattedData").innerHTML +=
+        "<p>" + jsonData[para] + "</p>";
+
+    let theNewEncrypt = document.getElementById("newEncrypt").value;
+
+    if (theNewEncrypt == "caesar") {
+
+        let shift = 3;
+        let encryptedText = "";
+
+        for (let i = 0; i < jsonData[para].length; i++) {
+
+            let code = jsonData[para].charCodeAt(i);
+
+            code = code + shift;
+
+            encryptedText = encryptedText + String.fromCharCode(code);
+        }
+
+        document.getElementById("myEncryptedData").innerHTML +=
+            "<p>" + encryptedText + "</p>";
     }
 
+    else if (theNewEncrypt == "vigenere") {
+
+    let key = "key";
+    let encryptedText = "";
+    let keyIndex = 0;
+
+    for (let i = 0; i < jsonData[para].length; i++) {
+
+        let code = jsonData[para].charCodeAt(i);
+
+        if (code >= 97 && code <= 122) {
+
+            let shift = key.charCodeAt(keyIndex % key.length) - 97;
+
+            code = code + shift;
+
+            if (code > 122) {
+                code = code - 26;
+            }
+
+            encryptedText = encryptedText + String.fromCharCode(code);
+
+            keyIndex++;
+        }
+        else {
+            encryptedText = encryptedText + jsonData[para][i];
+        }
+    }
+
+    document.getElementById("myEncryptedData").innerHTML +=
+        "<p>" + encryptedText + "</p>";
+}
+}
   return true;
 }
