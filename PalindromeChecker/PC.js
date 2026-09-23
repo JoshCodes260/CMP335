7d760d2b4a58439442eb5f73db5ba2ab090347bd
JoshCodes <Joshua.kleinsasser@doane.edu>
2026-09-23 11:40:37 -0500

    PC v1

---
+++ PalindromeChecker/PC.js
@@ -0,0 +1,157 @@
+function checkWord() {
+
+    // Get the word from the form
+    let word = document.forms["rangeForm"]["newWord"].value;
+
+    // Get the algorithm choice
+    let algorithm = document.forms["rangeForm"]["algorithm"].value;
+
+
+    // Check if a word was entered
+    if (word == "") {
+
+        alert("Please enter a word.");
+
+        return;
+    }
+
+
+    // Check if an algorithm was picked
+    if (algorithm == "") {
+
+        alert("Please select an algorithm.");
+
+        return;
+    }
+
+
+
+    let answer;
+
+
+    // Use Algorithm 1
+    if (algorithm == "1") {
+
+        answer = algorithm1(word);
+
+    }
+
+
+    // Use Algorithm 2
+    if (algorithm == "2") {
+
+        answer = algorithm2(word);
+
+    }
+
+
+    // Make an object
+    let result = {
+
+        word: word,
+
+        algorithm: algorithm,
+
+        palindrome: answer
+
+    };
+
+
+    // Put result in Algorithm 1 list
+    if (algorithm == "1") {
+
+        document.getElementById("algorithm1List").innerHTML =
+    document.getElementById("algorithm1List").innerHTML +
+    result.word + ": " + result.palindrome + "<br>";
+
+    }
+
+
+    // Put result in Algorithm 2 list
+    if (algorithm == "2") {
+
+        document.getElementById("algorithm2List").innerHTML =
+    document.getElementById("algorithm2List").innerHTML +
+    result.word + ": " + result.palindrome + "<br>";
+
+    }
+
+}
+
+
+
+function algorithm1(word) {
+
+    let backwards = "";
+
+
+    // Start at the last letter and move backwards
+    for (let i = word.length - 1; i >= 0; i--) {
+
+        backwards = backwards + word[i];
+
+    }
+
+
+    // Compare the word to the backwards word
+    if (word == backwards) {
+
+        return true;
+
+    }
+    else {
+
+        return false;
+
+    }
+
+}
+
+
+
+function algorithm2(word) {
+
+    let first = 0;
+
+    let last = word.length - 1;
+
+
+    // Keep checking letters until we reach the middle
+    while (first < last) {
+
+
+        // If the letters do not match
+        if (word[first] != word[last]) {
+
+            return false;
+
+        }
+
+
+        // Move toward the middle
+        first = first + 1;
+
+        last = last - 1;
+
+    }
+
+
+    return true;
+
+}
+
+
+
+function clearList1() {
+
+    document.getElementById("algorithm1List").innerHTML = "";
+
+}
+
+
+
+function clearList2() {
+
+    document.getElementById("algorithm2List").innerHTML = "";
+
+}
